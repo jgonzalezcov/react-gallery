d5b2fb815e98a94b764ff0203a956ca030c2caa4
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-08-18 10:18:44 -0400

    feat: App.js file is modified, with the code to call the components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Gallery from './components/Gallery.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import React from 'react'
+import ArrayInfo from './components/ArrayInfo.jsx'
+
+const heroeImages = require.context('./assets/img', true)
+function App() {
+  return (
+    <div>
+      <Header
+        title="Galería Dragon Ball Super (ReactJs)"
+        alignment="center"
+        classname="header_title"
+      />
+      <div className="card_container">
+        {ArrayInfo.map((e) => (
+          <Gallery
+            key={e.id}
+            srcs={heroeImages(e.srcs)}
+            alt={e.alt}
+            name={e.nombre}
+            category={e.raza}
+          />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+export default App
